76e4696b1a20e837153f130d101e60b72bf14275
0xAurelou <aurelien@fuzzinglabs.com>
2022-10-05 16:11:34 +0200

    ADD manual solution update, no more ABI requires and riddle is a template 😀

--- manual-solutions/riddle.py
+++ manual-solutions/riddle.py
@@ -1,51 +1,46 @@
-import asyncio
-from pathlib import Path
-
+from unittest import result
 from starknet_py.contract import Contract
 from starknet_py.net import AccountClient, KeyPair
 from starknet_py.net.gateway_client import GatewayClient
 from starknet_py.net.models.chains import StarknetChainId
-from starknet_py.net.networks import MAINNET, TESTNET
+from starknet_py.net.networks import TESTNET
 from starknet_py.net.signer.stark_curve_signer import StarkCurveSigner
-from starkware.python.utils import to_bytes
 from starkware.starknet.core.os.contract_address.contract_address import \
     calculate_contract_address_from_hash
+from starkware.crypto.signature.signature import private_to_stark_key
+from starknet_py.net.signer.stark_curve_signer import StarkCurveSigner
 
-MAX_LEN_FELT = 31
-testnet = "testnet"
-chain_id = StarknetChainId.TESTNET
+def felt_to_str(felt):
+    length = (felt.bit_length() + 7) // 8
+    return felt.to_bytes(length, byteorder="big").decode("utf-8")
 
+if __name__ == "__main__":
 
-def str_to_felt(text):
-    if len(text) > MAX_LEN_FELT:
-        raise Exception("Text length too long to convert to felt.")
+    # TO CHANGE /*
 
-    return int.from_bytes(text.encode(), "big")
+    # Update your info after running ./run.sh riddle-of-sphinx 31337 5050 in a terminal (don't shutdown it)
+    # And in an another terminal do nc localhost 31137, this will show you an interface, press 1, 1.
+    # This will show you can all the variable use in this contract, copy paste them here.
+    uuid="7b4349b3-2339-47de-99ac-d9742920ac34"
+    rpc_endpoint="http://7b4349b3-2339-47de-99ac-d9742920ac34@127.0.0.1:5050"
+    private_key="0xa7b5f4033a7470fbdc8e5f1d62391b97"
+    player_address="3493649071163658078167914646442170644288368666862236563148118203450221157864"
+    contract=("0x78fe8c8dae0ed159d978edd6b9e69b9f5626b6053bbf3a1b5af2fa5a05140cc")
 
+    # TO CHANGE */
 
-async def main():
-    node_url = "http://3a666138-b77a-4251-9e73-4896f7e05a6c@127.0.0.1:5050"
-    contractAddress = (
-        "0x2377325ddc7575c83eb51d2b223b53b9a834726d26c4a72f2f4aaf1a8f881b1"
+    prvkey=int(private_key, 16)
+    pubkey=private_to_stark_key(prvkey)
+    key_pair=KeyPair(private_key=prvkey, public_key=pubkey)
+    player_address=calculate_contract_address_from_hash(
+        salt=20,
+        class_hash=1803505466663265559571280894381905521939782500874858933595227108099796801620,
+        constructor_calldata=[pubkey],
+        deployer_address=0
     )
-    private_key = "0xf23a838c17900bb04f8337c3cbc40ef3"
-    player_address = 1061187669397351790558506865006261457388527257759064434023491866176219505681
 
-    gateway_client = GatewayClient(node_url, TESTNET)
-
-    key_pair = KeyPair.from_private_key(key=int(private_key, 16))
-    print(key_pair)
-
-    player_public_key = key_pair.public_key
-    print("Player public key", player_public_key)
-
-    # player_address = calculate_contract_address_from_hash(
-    #     salt=20,
-    #     class_hash=1803505466663265559571280894381905521939782500874858933595227108099796801620,
-    #     constructor_calldata=[player_public_key],
-    #     deployer_address=0,
-    # )
-    # print("Player address", player_address, hex(player_address))
+    gateway_client = GatewayClient(rpc_endpoint, TESTNET)
+    print("Player address", player_address, hex(player_address))
 
     signer = StarkCurveSigner(player_address, key_pair, StarknetChainId.TESTNET)
     account_client = AccountClient(
@@ -55,44 +50,18 @@ async def main():
         supported_tx_version=1,
     )
 
-    riddle_contract = Contract(
-        address=contractAddress, abi=riddleABI, client=account_client
-    )
-    solution = (await riddle_contract.functions["solution"].call()).solution
-    print("Current solution", solution)
+    c = Contract.from_address_sync(contract, account_client)
 
-    input_string = str_to_felt("man")
-    response = await account_client.execute(
-        calls=[riddle_contract.functions["solve"].prepare(input_string)],
-        max_fee=int(1e16),
-    )
-    status = await account_client.wait_for_tx(response.transaction_hash)
-    print(status)
-    print(response)
+    # TO CHANGE /*
 
-    solution = (await riddle_contract.functions["solution"].call()).solution
+    print("Before solve call")
+    print(c.functions["solution"].call_sync())
+    c.functions["solve"].invoke_sync("man", max_fee=0)
+    result = c.functions["solution"].call_sync()
+    print("After solve call")
+    if felt_to_str(getattr(result, "solution")) == "man" or getattr(result, "solution") == 7168366:
+        print("You found the solution:", felt_to_str(getattr(result, "solution")))
+    else:
+        print("Try again you didn't find the solution")
 
-    print("New solution", solution)
-    if to_bytes(solution).lstrip(b"\x00") == b"man":
-        print("SOLVED!")
-
-
-riddleABI = [
-    {
-        "inputs": [{"name": "solution", "type": "felt"}],
-        "name": "solve",
-        "outputs": [],
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "solution",
-        "outputs": [{"name": "solution", "type": "felt"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-]
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    # TO CHANGE
