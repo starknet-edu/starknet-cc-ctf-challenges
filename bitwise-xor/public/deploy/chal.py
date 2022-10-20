479d15ff2529e1c72de8096b58c51fd82a46199a
0xAurelou <aurelien@fuzzinglabs.com>
2022-10-20 15:35:27 +0200

    FIX bitwise error

--- bitwise-xor/public/deploy/chal.py
+++ bitwise-xor/public/deploy/chal.py
@@ -7,13 +7,15 @@ from starknet_py.contract import Contract
 from starkware.python.utils import to_bytes
 
 async def deploy(client: AccountClient, player_address: int) -> int:
-    print("[+] deploying bitwise")
+    print("[+] deploying bitwise-xor")
     bitwise_deployement = await Contract.deploy(
         client=client,
         compiled_contract=Path("compiled/bitwise-xor.json").read_text(),
         constructor_args=[],
     )
     await bitwise_deployement.wait_for_acceptance()
+    return bitwise_deployement.deployed_contract.address
+
 
 async def checker(client: AccountClient, bitwise_contract: Contract, player_address: int) -> bool:
     solution = (await bitwise_contract.functions["is_challenge_done"].call()).res
