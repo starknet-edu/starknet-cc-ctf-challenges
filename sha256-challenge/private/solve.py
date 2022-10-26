4fb81c14ac5b938144813d88d4a0ebff0c60f3ef
0xAurelou <aurelien@fuzzinglabs.com>
2022-10-26 17:45:12 +0200

    ADD private solution sha256

--- sha256-challenge/private/solve.py
+++ sha256-challenge/private/solve.py
@@ -6,7 +6,7 @@ from starknet_py.contract import Contract
 from starkware.python.utils import from_bytes
 
 async def solver(client: AccountClient, riddle_contract: Contract):
-    result = await riddle_contract.functions["test_password"].invoke([2003329396,1634168687,1684109614], 12, max_fee=int(1e16))
+    result = await riddle_contract.functions["test_password"].invoke([1735355492,1718186856], 12, max_fee=int(1e16))
     await result.wait_for_acceptance()
 
 run_solver(solver)
